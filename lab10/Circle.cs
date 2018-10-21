61739a8b45887a998e58b313e4a2244b0dd55343
Mauricio Reyna <mauriciojreyna@gmail.com>
2018-10-21 15:10:36 -0400

    class commit

---
+++ lab10/Circle.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab10
+{
+    public class Circle
+    {
+        private double radius;
+        public double Radius
+        {
+            get
+            {
+                return radius;
+            }
+            set
+            {
+                radius = value;
+            }
+        }
+        public Circle(double radius)
+        {
+            this.radius = radius;
+        }
+        public double CalculateCircumference()
+        {
+            return 2 * System.Math.PI * radius;
+        }
+        public string CalculateFormattedCircumference()
+        {
+            return FormatNumber(CalculateCircumference());
+        }
+        public double CalculateArea()
+        {
+            return System.Math.PI * radius * radius;
+        }
+        public string CalculateFormattedArea()
+        {
+            return FormatNumber(CalculateArea());
+        }
+        private string FormatNumber(double x)
+        {
+            return x.ToString("0.##");
+        }
+    }
+}
--- lab10/Program.cs
+++ lab10/Program.cs
@@ -10,6 +10,12 @@ namespace lab10
     {
         static void Main(string[] args)
         {
+            //prompt user to enter radius
+            Console.WriteLine("");
+            //display error if user input is invalid
+            //once valid data is input calculate area and circumference. Round to the nearest 2 decimal places.
+            //prompt user to continue
+            //say goodbye and include how many circles were made
         }
     }
 }
