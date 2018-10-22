8b6c930394dcc021f519c827a2561abf5bd1ba8f
Mauricio Reyna <mauriciojreyna@gmail.com>
2018-10-21 21:44:15 -0400

    almost done

--- lab10/Program.cs
+++ lab10/Program.cs
@@ -10,12 +10,53 @@ namespace lab10
     {
         static void Main(string[] args)
         {
-            //prompt user to enter radius
-            Console.WriteLine("");
-            //display error if user input is invalid
-            //once valid data is input calculate area and circumference. Round to the nearest 2 decimal places.
-            //prompt user to continue
-            //say goodbye and include how many circles were made
-        }
+            bool shouldContinue = false;
+            bool isInputValid = false;
+            string input = null;
+            double radius = 0;
+
+            Console.WriteLine("Welcome to the Circle Tester");
+
+            do
+            {
+                shouldContinue = false;
+                do
+                {
+                    try
+                    {
+                        isInputValid = true;
+                        Console.Write("Enter radius: ");
+                        input = Console.ReadLine();
+                        radius = double.Parse(input);
+                    }
+                    catch (FormatException)
+                    {
+                        Console.WriteLine("That input is invalid");
+                        isInputValid = false;
+                    }
+                } while (!isInputValid);
+
+                Circle c = new Circle(radius);
+                c.CalculateFormattedCircumference();
+                Console.WriteLine(c.CalculateFormattedCircumference());
+                c.CalculateFormattedArea();
+                Console.WriteLine(c.CalculateFormattedArea());
+
+                Console.WriteLine("Continue? (yes/no)");
+                input = Console.ReadLine();
+                if (input.Equals("yes", StringComparison.InvariantCultureIgnoreCase))
+                {
+                    shouldContinue = true;
+                }
+
+            } while (shouldContinue);
+            Console.ReadKey();
+        }   
     }
 }
+
+
+//display error if user input is invalid
+//once valid data is input calculate area and circumference. Round to the nearest 2 decimal places.
+//prompt user to continue
+//say goodbye and include how many circles were made
